2f5bbcc7a54a045b2c720d6970683079741c5bdf
Eduardo-Barraque <eduabarraque@gmail.com>
2021-11-16 22:00:46 -0300

    lendo json, classe produto e produto_dao feitas

--- application/model/dao/produto_dao.py
+++ application/model/dao/produto_dao.py
@@ -1,5 +1,28 @@
+from application.model.entity.produto import Produto
 import json
+class Produto_dao:
+    def __init__(self):
+        self._lista_produtos = []
+        with open("products.json") as documento:
+            produtos = json.load( documento)
+            documento.close()
 
-with open(url_for('static',filename="products.json")) as jsonFile:
-    jsonObject = json.load(jsonFile)
-    jsonFile.close()
+        for i in produtos:
+            id = produtos["id"]
+            nome = produtos["nome"]
+            imagem = produtos["imagem"]
+            oldprice = produtos["oldprice"]
+            price = produtos["price"]
+            description = produtos["description"]
+            carregar_produto = Produto(
+                '{}'.format(id),
+                '{}'.format(nome),
+                '{}'.format(imagem),
+                '{}'.format(oldprice),
+                '{}'.format(price),
+                '{}'.format(description))
+            self.__lista_produtos.append(carregar_produto)
+        
+            
+
+
--- application/model/entity/produto.py
+++ application/model/entity/produto.py
@@ -1,3 +1,50 @@
 class Produto:
-    def __init__(self):
-        pass
+    def __init__( self, id:int, nome:str, imagem:str, oldprice:int, price:int, description:str,):
+        self.__nome = nome
+        self.__id = id
+        self.__imagem = imagem
+        self.__oldprice = oldprice
+        self.__price = price
+        self.__description = description
+    
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, value):
+        self.__nome = value
+
+    @property
+    def imagem(self):
+        return self.__imagem
+    @imagem.setter
+    def imagem(self, value):
+        self.__imagem = value
+
+    @property
+    def oldprice(self):
+        return self.__oldprice
+    @oldprice.setter
+    def oldprice(self, value):
+        self.__oldprice = value
+
+    @property
+    def price(self):
+        return self.__price
+    @price.setter
+    def price(self, value):
+        self.__price = value
+
+    @property
+    def description(self):
+        return self.__description
+    @description.setter
+    def description(self, value):
+        self.__description = value
